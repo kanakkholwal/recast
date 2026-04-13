ae6638d89a8892f9bf53e6d1d23d55918714db7e
kanakkholwal <kanakkholwal@gmail.com>
2026-04-13 23:28:18 +0530

    Refactor code structure for improved readability and maintainability

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,25 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-cloudflare';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
 		alias: {
 			$components: 'src/components',
 			$utils: 'src/utils',
@@ -28,7 +13,32 @@ const config = {
 			$tools: 'src/tools',
 			$stores: 'src/stores',
 			"@": "./src/@",
-		}
+		},
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		}),
+		// cloudflare
+			// adapter: adapter({
+		// 	// See below for an explanation of these options
+		// 	config: undefined,
+		// 	platformProxy: {
+		// 		configPath: undefined,
+		// 		environment: undefined,
+		// 		persist: undefined
+		// 	},
+		// 	fallback: 'plaintext',
+		// 	routes: {
+		// 		include: ['/*'],
+		// 		exclude: ['<all>']
+		// 	}
+		// }),
 	}
 };
 
